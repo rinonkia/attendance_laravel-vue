bcea3eb764e02805f900bc8921d302c46607465f
rinonkia <git@gatapon.net>
2019-05-07 23:59:29 +0900

    restart

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component. <br>
+                        <span class="test">{{ test }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.test {
+    color: red;
+}
+</style>
+
+<script>
+export default {
+    props: {
+        test: String,
+    },
+
+    mounted() {
+        console.log('ExampleComponent mounted.')
+    },
+}
+</script>
